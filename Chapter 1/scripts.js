47adf9b0b297a7577a490fa689468b40ca052ae6
DaniAkash <s.daniakash@gmail.com>
2017-04-28 23:05:16 +0530

    load tasks html

--- Chapter 1/scripts.js
+++ Chapter 1/scripts.js
@@ -1,9 +1,40 @@
 class ToDoClass {
     constructor() {
-        alert('Hello World!');
+        this.tasks = [
+            {task: 'Go to Dentist', isComplete: false},
+            {task: 'Do Gardening', isComplete: true},
+            {task: 'Renew Library Account', isComplete: false},
+        ];
+
+        this.loadTasks();
+    }
+
+    generateTaskHtml(task, index) {
+        return `
+            <li class="list-group-item checkbox">
+                <div class="row">
+                    <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
+                        <label><input id="toggleTaskStatus" type="checkbox" onchange="toDo.toggleTaskStatus(${index})" value="" class="" ${task.isComplete?'checked':''}></label>
+                    </div>
+                    <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${task.isComplete?'complete':''}">
+                        ${task.task}
+                    </div>
+                    <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
+                        <a class="" href="/" onClick="toDo.deleteTask(event, ${index})"><i id="deleteTask" data-id="${index}" class="delete-icon glyphicon glyphicon-trash"></i></a>
+                    </div>
+                </div>
+            </li>
+        `;
+    }
+
+    loadTasks() {
+        let tasksHtml = this.tasks.reduce((html, task, index) => html += this.generateTaskHtml(task, index), '');
+        document.getElementById('taskList').innerHTML = tasksHtml;
     }
 }
 
-window.addEventListener("load", function () {
-  var toDo = new ToDoClass()
+let toDo;
+
+window.addEventListener("load", () => {
+  toDo = new ToDoClass();
 });
