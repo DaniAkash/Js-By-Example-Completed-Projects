df836dadcf886be7c05437a87784dc333a93b3c6
DaniAkash <s.daniakash@gmail.com>
2017-04-30 14:36:53 +0530

    fix alignments

--- Chapter 1/scripts.js
+++ Chapter 1/scripts.js
@@ -1,46 +1,46 @@
 class ToDoClass {
     constructor() {
-        this.tasks = [
-            {task: 'Go to Dentist', isComplete: false},
-            {task: 'Do Gardening', isComplete: true},
-            {task: 'Renew Library Account', isComplete: false},
-        ];
+      this.tasks = [
+        {task: 'Go to Dentist', isComplete: false},
+        {task: 'Do Gardening', isComplete: true},
+        {task: 'Renew Library Account', isComplete: false},
+      ];
 
-        this.loadTasks();
+      this.loadTasks();
     }
 
     toggleTaskStatus(index) {
-        this.tasks[index].isComplete = !this.tasks[index].isComplete;
-        this.loadTasks();
+      this.tasks[index].isComplete = !this.tasks[index].isComplete;
+      this.loadTasks();
     }
 
     deleteTask(event, taskIndex) {
-        event.preventDefault();
-        this.tasks.splice(taskIndex, 1);
-        this.loadTasks();
+      event.preventDefault();
+      this.tasks.splice(taskIndex, 1);
+      this.loadTasks();
     }
 
     generateTaskHtml(task, index) {
-        return `
-            <li class="list-group-item checkbox">
-                <div class="row">
-                    <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
-                        <label><input id="toggleTaskStatus" type="checkbox" onchange="toDo.toggleTaskStatus(${index})" value="" class="" ${task.isComplete?'checked':''}></label>
-                    </div>
-                    <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${task.isComplete?'complete':''}">
-                        ${task.task}
-                    </div>
-                    <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
-                        <a class="" href="/" onClick="toDo.deleteTask(event, ${index})"><i id="deleteTask" data-id="${index}" class="delete-icon glyphicon glyphicon-trash"></i></a>
-                    </div>
-                </div>
-            </li>
-        `;
+      return `
+        <li class="list-group-item checkbox">
+          <div class="row">
+            <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
+              <label><input id="toggleTaskStatus" type="checkbox" onchange="toDo.toggleTaskStatus(${index})" value="" class="" ${task.isComplete?'checked':''}></label>
+            </div>
+            <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${task.isComplete?'complete':''}">
+              ${task.task}
+            </div>
+            <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
+              <a class="" href="/" onClick="toDo.deleteTask(event, ${index})"><i id="deleteTask" data-id="${index}" class="delete-icon glyphicon glyphicon-trash"></i></a>
+            </div>
+          </div>
+        </li>
+      `;
     }
 
     loadTasks() {
-        let tasksHtml = this.tasks.reduce((html, task, index) => html += this.generateTaskHtml(task, index), '');
-        document.getElementById('taskList').innerHTML = tasksHtml;
+      let tasksHtml = this.tasks.reduce((html, task, index) => html += this.generateTaskHtml(task, index), '');
+      document.getElementById('taskList').innerHTML = tasksHtml;
     }
 }
 
