9538c4488b901679c2601e071b69c4187fa71496
DaniAkash <s.daniakash@gmail.com>
2017-05-15 01:03:41 +0530

    webpack cache busting

--- Chapter 2/webpack.config.js
+++ Chapter 2/webpack.config.js
@@ -10,10 +10,15 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const isProduction = (process.env.NODE_ENV === 'production');
 
 /**
+ * Include hash to filenames for cache busting - only at production
+ */
+const fileNamePrefix = isProduction? '[chunkhash].' : '';
+
+/**
  * An instance of ExtractTextPlugin
  */
 const extractLess = new ExtractTextPlugin({
-    filename: "[name].css",
+    filename: fileNamePrefix + "[name].css",
 });
 
 module.exports = {
@@ -24,7 +29,7 @@ module.exports = {
   },
   output: {
       path: __dirname + "/dist",
-      filename: '[name].js',
+      filename: fileNamePrefix + '[name].js',
       publicPath: '/dist/',
   },
   devServer: { // Configuration for webpack-dev-server
@@ -103,8 +108,17 @@ module.exports = {
       paths: glob.sync(__dirname + '/*.html'),
       minimize: true,
     }),
-    new webpack.HotModuleReplacementPlugin(),
   ],
+}
+
+
+/**
+ * Non-Production plugins
+ */
+if(!isProduction) {
+  module.exports.plugins.push(
+    new webpack.HotModuleReplacementPlugin() // HMR plugin will cause problems with [chunkhash]
+  );
 }
 
 /**
@@ -112,6 +126,14 @@ module.exports = {
  */
 if(isProduction) {
   module.exports.plugins.push(
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin(),
+    function() { // Create a manifest.json file that contain the hashed file names of generated static resources
+      this.plugin("done", function(status) {
+        require("fs").writeFileSync(
+          __dirname + "/dist/manifest.json",
+          JSON.stringify(status.toJson().assetsByChunkName)
+        );
+      });
+    }
   );
 }
