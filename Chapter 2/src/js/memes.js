aa2f63dfc0ccd9d6fa87df13354e8906d06c29c8
DaniAkash <s.daniakash@gmail.com>
2017-05-17 22:41:27 +0530

    create canvas

--- Chapter 2/src/js/memes.js
+++ Chapter 2/src/js/memes.js
@@ -1,10 +1,26 @@
 import './general';
 
-console.log('Memes JS file');
+const deviceWidth = window.innerWidth;
+
 class Memes {
   constructor() {
-    console.log('Inside Memes class');
+      this.$canvas = document.querySelector('#imgCanvas');
+      this.$topTextInput = document.querySelector('#topText');
+      this.$bottomTextInput = document.querySelector('#bottomText');
+      this.$imageInput = document.querySelector('#image');
+      this.$downloadButton = document.querySelector('#downloadMeme');
+
+      this.createCanvas();
   }
+
+  createCanvas() {
+    let canvasHeight = Math.min(480, deviceWidth-30);
+    let canvasWidth = Math.min(640, deviceWidth-30);
+
+    this.$canvas.height = canvasHeight;
+    this.$canvas.width = canvasWidth;
+  }
+
 }
 
 new Memes();
