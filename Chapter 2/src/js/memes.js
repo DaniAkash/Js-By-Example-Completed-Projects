b0e4d041ff813952deda466d050e48a9ad1592b6
DaniAkash <s.daniakash@gmail.com>
2017-05-19 08:36:38 +0530

    FileReader and Image objects

--- Chapter 2/src/js/memes.js
+++ Chapter 2/src/js/memes.js
@@ -26,7 +26,38 @@ class Memes {
     let context = this.$canvas.getContext('2d');
 
     if (this.$imageInput.files && this.$imageInput.files[0]) {
-      console.log('cool');
+      let reader = new FileReader();
+
+      reader.onload = () => {
+        let image = new Image();
+
+        image.onload = () => {
+          // this.$canvas.height = image.height;
+          // this.$canvas.width = image.width;
+          // context.clearRect(0, 0, this.$canvas.height, this.$canvasWidth);
+          // context.drawImage(image,0,0);
+
+          // let fontSize = ((this.$canvas.width+this.$canvas.height)/2)*4/100;
+          // context.lineWidth  = fontSize/5;
+          // context.font = `${fontSize}pt sans-serif`;
+          // context.strokeStyle = 'black';
+          // context.fillStyle = 'white';
+          // context.lineJoin = 'round';
+          // context.textAlign = 'center';
+          // context.textBaseline = 'top';
+
+          // context.strokeText(this.$topTextInput.value.toUpperCase(), this.$canvas.width/2, this.$canvas.height*(5/100));
+          // context.fillText(this.$topTextInput.value.toUpperCase(), this.$canvas.width/2, this.$canvas.height*(5/100));
+
+          // context.strokeText(this.$bottomTextInput.value.toUpperCase(), this.$canvas.width/2, this.$canvas.height*(90/100));
+          // context.fillText(this.$bottomTextInput.value.toUpperCase(), this.$canvas.width/2, this.$canvas.height*(90/100));
+        };
+
+        image.src = reader.result;
+
+      };
+
+      reader.readAsDataURL(this.$imageInput.files[0]);
     }
   }
 
