c8f9089708926384cf838169b390ca472173cb76
DaniAkash <s.daniakash@gmail.com>
2017-06-02 03:33:39 +0530

    create references to DOM elements

--- Chapter 3/src/js/home.js
+++ Chapter 3/src/js/home.js
@@ -1,6 +1,19 @@
 import './general';
 
 class Home {
+  constructor() {
+    this.$form = document.querySelector('#registrationForm');
+    this.$username = document.querySelector('#username');
+    this.$email = document.querySelector('#email');
+    this.$phone = document.querySelector('#phone');
+    this.$age = document.querySelector('#age');
+    this.$profession = document.querySelector('#profession');
+    this.$experience = document.querySelector('#experience');
+    this.$comment = document.querySelector('#comment');
+    this.$submit = document.querySelector('#submit');
+    this.$loadingIndicator = document.querySelector('#loadingIndicator');
+  }
+
 
 }
 
