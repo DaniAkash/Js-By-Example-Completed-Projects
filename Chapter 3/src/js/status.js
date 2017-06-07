d25258c654afbf61ea5c57d3dbfd5a67839fa8cc
DaniAkash <s.daniakash@gmail.com>
2017-06-07 05:34:53 +0530

    create Status class

--- Chapter 3/src/js/status.js
+++ Chapter 3/src/js/status.js
@@ -1,1 +1,20 @@
 import './general';
+
+class Status {
+  constructor() {
+    this.$experienceTab = document.querySelector('#experienceTab');
+    this.$professionTab = document.querySelector('#professionTab');
+    this.$ageTab = document.querySelector('#ageTab');
+
+    this.$ageCanvas = document.querySelector('#ageChart');
+    this.$professionCanvas = document.querySelector('#professionChart');
+    this.$experienceCanvas = document.querySelector('#experienceChart');
+
+    this.$loadingIndicator = document.querySelector('#loadingIndicator');
+    this.$tabArea = document.querySelector('#tabArea');
+    this.$chartArea = document.querySelector('#chartArea');
+
+    this.statisticData;
+  }
+
+}
