35babb54cb14f29d5abab8a95b33bedaabffbd98
DaniAkash <s.daniakash@gmail.com>
2017-06-12 00:40:05 +0530

    Add Google map with marker

--- Chapter 3/src/js/about.js
+++ Chapter 3/src/js/about.js
@@ -1,7 +1,27 @@
 import './general';
 
 export function initMap() {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {lat: 59.325, lng: 18.070}
+  });
 
+  const marker = new google.maps.Marker({
+    map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: {lat: 59.325, lng: 18.070}
+  });
+
+  marker.addListener('click', () => {
+    infowindow.open(map,marker);
+  });
+
+  const infowindow = new google.maps.InfoWindow({
+      content: "<h3>Event Location</h3><p>Event Address with all the contact details</p>"
+  });
+
+  infowindow.open(map,marker);
 }
 
 window.addEventListener("load", () => {
