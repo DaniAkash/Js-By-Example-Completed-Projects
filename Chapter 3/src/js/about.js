9d019e5c45c9f156576208276d211c523c8636ab
DaniAkash <s.daniakash@gmail.com>
2017-06-11 23:44:15 +0530

    add GMAP script

--- Chapter 3/src/js/about.js
+++ Chapter 3/src/js/about.js
@@ -1,1 +1,11 @@
 import './general';
+
+export function initMap() {
+
+}
+
+window.addEventListener("load", () => {
+  const $script = document.createElement('script');
+  $script.src = `https://maps.googleapis.com/maps/api/js?key=${GMAP_KEY}&callback=bundle.initMap`;
+  document.querySelector('body').appendChild($script);
+});
