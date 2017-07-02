1c8ead1955932b217e6f5be62754115d7cdc9fa1
DaniAkash <s.daniakash@gmail.com>
2017-07-02 13:20:19 +0530

    add class variables

--- Chapter 4/src/js/home.js
+++ Chapter 4/src/js/home.js
@@ -10,7 +10,21 @@ const webrtc = new SimpleWebRTC({
 
 class Home {
   constructor() {
+    this.roomName = '';
 
+    this.$createRoomSection = document.querySelector('#createRoomSection');
+    this.$createRoomButton = document.querySelector('#createRoom');
+    this.$roomNameInput = document.querySelector('#roomNameInput');
+
+    this.$infoSection = document.querySelector('#infoSection');
+    this.$roomName = document.querySelector('#roomNameText');
+    this.$roomUrl = document.querySelector('#roomUrl');
+    this.$buttonArea = document.querySelector('.room-text');
+    this.$copy = document.querySelector('.copy');
+    this.$copied = document.querySelector('.copied');
+
+    this.$remotes = document.querySelector('.video-area');
+    this.$localVideo = document.querySelector('#localVideo');
   }
 
 }
