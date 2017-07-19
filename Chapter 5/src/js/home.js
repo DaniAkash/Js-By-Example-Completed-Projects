dc1d1e7aa3c4b0d4c896cce4729207560723e4b8
DaniAkash <s.daniakash@gmail.com>
2017-07-19 22:51:29 +0530

    get user's location

--- Chapter 5/src/js/home.js
+++ Chapter 5/src/js/home.js
@@ -5,5 +5,21 @@ window.addEventListener('load', () => {
 });
 
 function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, errorPosition);
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
+}
 
+function showPosition(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  console.log(latitude);
+  console.log(longitude);
+}
+
+function errorPosition(error) {
+  console.error(error);
 }
