992ac4c36485b1fd5360312abb1e5b90b45c0c6c
DaniAkash <s.daniakash@gmail.com>
2017-08-03 04:26:15 +0530

    install react strap and create header

--- Chapter 6 Blog/src/App.js
+++ Chapter 6 Blog/src/App.js
@@ -1,31 +1,47 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <nav className="navbar navbar-inverse navbar-fixed-top">
-          <div className="container">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-              <a className="navbar-brand" href="/">Event Registration</a>
-            </div>
-            <div id="navbar" className="collapse navbar-collapse">
-              <ul className="nav navbar-nav">
-                <li className="active"><a href="/">Home</a></li>
-                <li><a href="status.html">Status</a></li>
-                <li><a href="about.html">About</a></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Navbar color="faded" light toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavLink className={'navbar-brand'} to="/home">Home</NavLink>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink className={'nav-link'} activeClassName={'active'} to="/home">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className={'nav-link'} activeClassName={'active'} to="/authors">Authors</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className={'nav-link'} activeClassName={'active'} to="/new-post">New Post</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
     );
   }
