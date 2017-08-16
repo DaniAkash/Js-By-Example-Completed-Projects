b49512b1db7a3b4f9f79ff9ed19764305f49dac6
DaniAkash <s.daniakash@gmail.com>
2017-08-16 18:13:17 +0530

    ajaxCallsReducer use spread operator

--- Chapter 7/src/redux/reducers/ajaxCallsReducer.js
+++ Chapter 7/src/redux/reducers/ajaxCallsReducer.js
@@ -5,114 +5,87 @@ const ajaxCallsReducer = (state = initialState.ajaxCalls, action) => {
   switch(action.type) {
 
     case actions.GET_POSTS_AJAX_CALL_START:
-      return Object.assign(
-        {},
-        state,
-        {
-          getAllPosts: {
-            loading: true,
-            hasError: false,
-          },
-        }
-      );
+      return {
+        ...state,
+        getAllPosts: {
+          loading: true,
+          hasError: false,
+        },
+      };
 
     case actions.GET_POSTS_AJAX_CALL_SUCCESS:
-      return Object.assign(
-        {},
-        state,
-        {
-          getAllPosts: {
-            loading: false,
-            hasError: false,
-          },
-        }
-      );
+      return {
+        ...state,
+        getAllPosts: {
+          loading: false,
+          hasError: false,
+        },
+      };
 
 
     case actions.GET_POSTS_AJAX_CALL_FAILURE:
-      return Object.assign(
-        {},
-        state,
-        {
-          getAllPosts: {
-            loading: false,
-            hasError: true,
-          },
-        }
-      );
+      return {
+        ...state,
+        getAllPosts: {
+          loading: false,
+          hasError: true,
+        },
+      };
 
 
     case actions.GET_AUTHORS_AJAX_CALL_START:
-      return Object.assign(
-        {},
-        state,
-        {
-          getAuthors: {
-            loading: true,
-            hasError: false,
-          },
-        }
-      );
+      return {
+        ...state,
+        getAuthors: {
+          loading: true,
+          hasError: false,
+        },
+      };
 
     case actions.GET_AUTHORS_AJAX_CALL_SUCCESS:
-      return Object.assign(
-        {},
-        state,
-        {
-          getAuthors: {
-            loading: false,
-            hasError: false,
-          },
-        }
-      );
+      return {
+        ...state,
+        getAuthors: {
+          loading: false,
+          hasError: false,
+        },
+      };
 
     case actions.GET_AUTHORS_AJAX_CALL_FAILURE:
-      return Object.assign(
-        {},
-        state,
-        {
-          getAuthors: {
-            loading: false,
-            hasError: true,
-          },
-        }
-      );
+      return {
+        ...state,
+        getAuthors: {
+          loading: false,
+          hasError: true,
+        },
+      };
 
     case actions.ADD_POST_AJAX_CALL_START:
-      return Object.assign(
-        {},
-        state,
-        {
-          addPost: {
-            loading: true,
-            hasError: false,
-          },
-        }
-      );
+      return {
+        ...state,
+        addPost: {
+          loading: true,
+          hasError: false,
+        },
+      };
 
     case actions.ADD_POST_AJAX_CALL_SUCCESS:
-      return Object.assign(
-        {},
-        state,
-        {
-          addPost: {
-            loading: false,
-            hasError: false,
-          },
-        }
-      );
+      return {
+        ...state,
+        addPost: {
+          loading: false,
+          hasError: false,
+        },
+      };
 
     case actions.ADD_POST_AJAX_CALL_FAILURE:
-      return Object.assign(
-        {},
-        state,
-        {
-          addPost: {
-            loading: false,
-            hasError: true,
-          },
-        }
-      );
+      return {
+        ...state,
+        addPost: {
+          loading: false,
+          hasError: true,
+        },
+      };
 
     default:
       return state;
