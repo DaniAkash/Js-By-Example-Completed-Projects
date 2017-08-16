077ee5cbd3edb4a49ceb5cb5caeb6fe0661f85b2
DaniAkash <s.daniakash@gmail.com>
2017-08-16 16:29:55 +0530

    post actions

--- Chapter 7/src/redux/actions/postActions.js
+++ Chapter 7/src/redux/actions/postActions.js
@@ -21,3 +21,28 @@ export const getAllPosts = () => {
   };
 
 };
+
+export const postsApiCallStart = () => {
+  return {
+    type: actions.GET_POSTS_AJAX_CALL_START,
+  };
+};
+
+export const postsApiCallSuccess = () => {
+  return {
+    type: actions.GET_POSTS_AJAX_CALL_SUCCESS,
+  };
+};
+
+export const postsApiCallFailure = () => {
+  return {
+    type: actions.GET_POSTS_AJAX_CALL_FAILURE,
+  };
+};
+
+export const getPosts = (posts) => {
+  return {
+    type: actions.GET_POSTS,
+    payload: { posts },
+  };
+};
